9406880339e37e2a08581a0306a4642d822abef9
Gloria Mutie <mwongeliglo15@gmail.com>
2020-09-07 22:20:35 +0300

    shifting letters adjusted

--- src/main/java/Cipher.java
+++ src/main/java/Cipher.java
@@ -26,10 +26,10 @@ public class Cipher {
         final int cipherShift = k % alphaLength;
 
         // we shift down to 0..25 for a..z
-        char shifted = (char) (c - asciiShift);
+        char shifted = (char) (c + asciiShift);
 
         // Here we rotate the letter and handle "wrap-around" for negatives and value >= 26
-        shifted = (char) ((shifted + cipherShift + alphaLength) % alphaLength);
+        shifted = (char) ((shifted - cipherShift - alphaLength) % alphaLength);
 
         // Here we shift back up to english characters
         return (char) (shifted + asciiShift);
